0eda7399a11ed13a9572e31b05a0fd75665783e4
Thomas Belpomo <thomas.belpomo@hotmail.fr>
2023-11-15 16:34:58 +0100

    Exercice terminé

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
+  get "tasks" => "tasks#index"
+  get "tasks/:id/edit" => "tasks#edit", as: :edit_task
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+  patch "tasks/:id" => "tasks#update"
+  get "tasks/:id" => "tasks#show", as: :task
+  delete "tasks/:id" => "tasks#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
